2d3d6887f12ad52fb2126f653a3f44b87ddd8d22
Nathaniel Spry <nspry1997@gmail.com>
2017-09-29 20:50:20 -0400

    tightened up files

---
+++ javascript/liri.js
@@ -0,0 +1,23 @@
+var keys = require("./keys.js");
+var arg = process.argv[2];
+
+function myTweets() {}
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+switch (arg) {
+	case "my-tweets":
+	console.log("switch case works");
+	break;
+
+	// case "spotify-this-song":
+	// deposit();
+	// break;
+
+	// case "movie-this":
+	// withdraw();
+	// break;
+
+	// case "do-what-it-says":
+	// lotto();
+	// break;
+}
